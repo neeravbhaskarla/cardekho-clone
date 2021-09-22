66913f61e2db3ec21eadc795eb2f196be3a3e526
Neerav Bhaskarla <neeravbhaskarla@Neeravs-MacBook-Air.local>
2021-09-22 09:56:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import CarsGroups from './components/CarsGroups/CarsGroups'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/Navbar/Navbar'
+import NewTech from './components/NewTech/NewTech'
+import SliderPage from './components/Slider/SliderPage'
+import {DATA, DATA1, DATA2} from './data/Data'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-page">
+      <Navbar/>
+      <SliderPage/>
+      <NewTech/>
+      <CarsGroups title="The most searched cars" data={DATA}/>
+      <CarsGroups title="Trusted used cars by budget" data = {DATA1}/>
+      <CarsGroups title="Latest Cars" data={DATA2}/>
+      <Footer/>
     </div>
-  );
+  )
 }
-
-export default App;
